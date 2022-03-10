b242dd0bae71859a1a23cc836b32bc05807b76e8
Vadim Boev <shevelev39@gmail.com>
2022-03-10 16:04:19 +0300

    Add interface and repos

---
+++ eMenu/Dish.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eMenu
+{
+    public class Dish
+    {
+
+        public string Name { get; } // Food names
+
+        public int Cost { get; } // Food cost
+
+        public bool Stock { get; }
+
+        public double Weight { get; }
+
+        public Dish(string name, int cost, bool stock, double weight)
+        {
+            Name = name;
+            Cost = cost;
+            Stock = stock;
+            Weight = weight;
+        }
+    }
+}
---
+++ eMenu/IDishRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eMenu
+{
+    interface IDishRepository
+    {
+        Dish[] GetByName(string namePart);
+    }
+}
